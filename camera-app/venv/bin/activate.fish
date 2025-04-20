e9c2b0b9d41560a60614cdde25d8ac86b50719c4
rhysc25 <rdc50@cam.ac.uk>
2025-04-20 19:05:09 +0100

    Fixed Virtual Environment

--- camera-app/venv/bin/activate.fish
+++ camera-app/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/rhys/tortoise-app/camera-app/venv
+set -gx VIRTUAL_ENV /home/rhys/rhyspi/tortoise-app/camera-app/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
